d19728b73919c2a843f554c140ac03b5b1f74901
Carlos Matallín Civera <carlos.matallin@shopify.com>
2021-10-28 00:25:23 -0400

    Notifications

--- src/types.ts
+++ src/types.ts
@@ -215,7 +215,9 @@ export interface ActionListSection {
   items: ActionListItemDescriptor[];
 }
 
-export interface NotificationListItemDescriptor extends Notification {
+export interface NotificationListItemDescriptor
+  extends BadgeAction,
+    Notification {
   /** Visually hidden text for screen readers */
   accessibilityLabel?: string;
   /** Additional hint text to display with item */
@@ -232,6 +234,8 @@ export interface NotificationListItemDescriptor extends Notification {
   active?: boolean;
   /** Defines a role for the action */
   role?: string;
+  /** Defines a role for the action */
+  onDismiss?(): void;
 }
 
 export interface NotificationListSection {
